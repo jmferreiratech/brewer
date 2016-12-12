74868f05db224aa97bc210052c4cf9decb90c9cc
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-11 22:31:09 -0300

    ViewResolver configuration with Thymeleaf

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,13 @@
 package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
 
+	@RequestMapping("cervejas/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
 }
