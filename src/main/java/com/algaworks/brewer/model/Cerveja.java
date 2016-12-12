c9bed915005b7e2a56f558daa82e40270d0ab7ff
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-11 22:54:02 -0300

    Starts beers registering

---
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -0,0 +1,22 @@
+package com.algaworks.brewer.model;
+
+public class Cerveja {
+	private String sku;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+}
