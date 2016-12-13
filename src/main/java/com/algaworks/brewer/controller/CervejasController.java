c5327667813b7cfdf1b5d1966b98b36cb28a4e0d
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-12 23:31:12 -0300

    Beers registering form prototyping

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -27,10 +27,4 @@ public class CervejasController {
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return "redirect:/cervejas/novo";
 	}
-
-	@RequestMapping("cervejas/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
-
 }
