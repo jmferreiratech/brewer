2e85582e7f5a2ee55ba12ff6698349b6ca11b27c
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-13 23:14:25 -0300

    Adds City registering form page

---
+++ src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CidadesController {
+
+	@RequestMapping("cidades/novo")
+	public String novo() {
+		return "cidade/CadastroCidade";
+	}
+}
