1fae4835fc036ac264e70ba5078849fe2a28788d
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-13 22:32:47 -0300

    Adds Users registering form page

---
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -0,0 +1,13 @@
+package com.algaworks.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuariosController {
+
+	@RequestMapping("usuarios/novo")
+	public String novo() {
+		return "usuario/CadastroUsuario";
+	}
+}
