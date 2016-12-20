5875e4670398ab53f80bf19ec8f3afb0a4198c3d
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-20 16:16:47 -0300

    Configures beer insertion and make transaction manual

--- src/main/java/com/algaworks/brewer/config/init/AppInitialiazer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitialiazer.java
@@ -1,17 +1,19 @@
 package com.algaworks.brewer.config.init;
 
 import javax.servlet.Filter;
+
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer.config.JPAConfig;
+import com.algaworks.brewer.config.ServiceConfig;
 import com.algaworks.brewer.config.WebConfig;
 
 public class AppInitialiazer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { JPAConfig.class };
+		return new Class<?>[] { JPAConfig.class, ServiceConfig.class };
 	}
 
 	@Override
--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -15,12 +15,15 @@ import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Origem;
 import com.algaworks.brewer.model.Sabor;
 import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.service.CadastroCervejaService;
 
 @Controller
 public class CervejasController {
 
 	@Autowired
 	private Estilos estilos;
+	@Autowired
+	private CadastroCervejaService cadastroCervejaService;
 
 	@RequestMapping("cervejas/novo")
 	public ModelAndView novo(Cerveja cerveja) {
@@ -32,11 +35,11 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "cervejas/novo", method = RequestMethod.POST)
-	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model,
-			RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
 		}
+		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return new ModelAndView("redirect:/cervejas/novo");
 	}
