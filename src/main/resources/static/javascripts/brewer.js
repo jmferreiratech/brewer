f1bd89b8695f96fa546468635362b2207852c6ca
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-21 11:39:20 -0300

    Adds big numbers formatters, fixes shown format and forces pt-BR locale

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,9 +1,13 @@
 $(function() {
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
+	decimal.maskMoney({
+		decimal : ',',
+		thousands : '.'
+	});
 
 	var plain = $('.js-plain');
 	plain.maskMoney({
-		precision : 0
+		precision : 0,
+		thousands : '.'
 	});
 });
