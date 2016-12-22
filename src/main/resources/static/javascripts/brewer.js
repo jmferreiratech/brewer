3fe5db1e91037e8c7b2f6baf6be62b3339571052
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-22 11:19:46 -0300

    Modularizes maskmoney js

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,15 +1,31 @@
-$(function() {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({
-		decimal : ',',
-		thousands : '.',
-		allowZero : true
-	});
+var Brewer = Brewer || {};
 
-	var plain = $('.js-plain');
-	plain.maskMoney({
-		precision : 0,
-		thousands : '.',
-		allowZero : true
-	});
+Brewer.MaskMoney = (function() {
+
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+
+	}
+
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({
+			decimal : ',',
+			thousands : '.',
+			allowZero : true
+		});
+		this.plain.maskMoney({
+			precision : 0,
+			thousands : '.',
+			allowZero : true
+		});
+
+	}
+
+	return MaskMoney;
+}());
+
+$(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
 });
