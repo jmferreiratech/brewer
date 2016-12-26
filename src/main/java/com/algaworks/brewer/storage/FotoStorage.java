599c7a9690c12d8e05344b401de000df99d55ded
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-26 10:48:10 -0300

    Saves temp photo

--- src/main/java/com/algaworks/brewer/storage/FotoStorage.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorage.java
@@ -4,5 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FotoStorage {
 
-	public void salvarTemporariamente(MultipartFile[] files);
+	public String salvarTemporariamente(MultipartFile[] files);
 }
--- src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
+++ src/main/java/com/algaworks/brewer/storage/local/FotoStorageLocal.java
@@ -2,9 +2,11 @@ package com.algaworks.brewer.storage.local;
 
 import static java.nio.file.FileSystems.getDefault;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +23,26 @@ public class FotoStorageLocal implements FotoStorage {
 	public FotoStorageLocal() {
 		this(getDefault().getPath(System.getProperty("user.home"), ".brewerfotos"));
 	}
-	
+
 	public FotoStorageLocal(Path path) {
 		this.local = path;
-		criarPastas();		
+		criarPastas();
+	}
+
+	@Override
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome = null;
+		if (null != files && 0 < files.length) {
+			MultipartFile arquivo = files[0];
+			novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(
+						this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+			} catch (IOException e) {
+				throw new RuntimeException("Erro salvando a foto na pasta temporária", e);
+			}
+		}
+		return novoNome;
 	}
 
 	private void criarPastas() {
@@ -43,8 +61,11 @@ public class FotoStorageLocal implements FotoStorage {
 
 	}
 
-	@Override
-	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println("Salvando temporariamente...");
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+		if (logger.isDebugEnabled()) {
+			logger.debug(String.format("Nome original: %s, novo nome: %s", nomeOriginal, novoNome));
+		}
+		return novoNome;
 	}
 }
