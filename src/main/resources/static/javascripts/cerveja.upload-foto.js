a34d43810768c10f46e8ea1f7353603bfc5fbe14
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-26 15:58:14 -0300

    Keeps loaded photo after validation error

--- src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -23,6 +23,12 @@ Brewer.UploadFoto = (function() {
 		}
 		UIkit.uploadSelect($('#upload-select'), settings);
 		UIkit.uploadDrop(this.uploadDrop, settings);
+		if (this.inputNomeFoto.val()) {
+			onUploadCompleto.call(this, {
+				nome : this.inputNomeFoto.val(),
+				contentType : this.inputContentType.val()
+			});
+		}
 	}
 
 	function onUploadCompleto(resposta) {
