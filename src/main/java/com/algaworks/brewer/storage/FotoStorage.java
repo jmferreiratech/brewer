f1e2e005625cb8f039e2d5e0c86a4f140b701388
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-26 10:27:30 -0300

    Creates folder to save photos

---
+++ src/main/java/com/algaworks/brewer/storage/FotoStorage.java
@@ -0,0 +1,8 @@
+package com.algaworks.brewer.storage;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FotoStorage {
+
+	public void salvarTemporariamente(MultipartFile[] files);
+}
--- src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -9,14 +9,17 @@ public class FotoStorageRunnable implements Runnable {
 
 	private MultipartFile[] files;
 	private DeferredResult<FotoDTO> resultado;
+	private FotoStorage fotoStorage;
 
-	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado, FotoStorage fotoStorage) {
 		this.files = files;
 		this.resultado = resultado;
+		this.fotoStorage = fotoStorage;
 	}
 
 	@Override
 	public void run() {
+		this.fotoStorage.salvarTemporariamente(files);
 		String nomeFoto = files[0].getOriginalFilename();
 		String contentType = files[0].getContentType();
 		resultado.setResult(new FotoDTO(nomeFoto, contentType));
