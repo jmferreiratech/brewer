6f9f20acfcf9cb918f409b09ba29679e07d7b2b7
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2016-12-27 20:25:10 -0300

    Implements beers search paging

--- src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/cerveja/CervejasImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
@@ -23,8 +24,15 @@ public class CervejasImpl implements CervejasQueries {
 	@SuppressWarnings("unchecked")
 	@Transactional(readOnly = true)
 	@Override
-	public List<Cerveja> filtrar(CervejaFilter filtro) {
+	public List<Cerveja> filtrar(CervejaFilter filtro, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cerveja.class);
+
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int paginaAtual = pageable.getPageNumber();
+		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
+
+		criteria.setMaxResults(totalRegistrosPorPagina);
+		criteria.setFirstResult(primeiroRegistro);
 		if (null != filtro) {
 			if (!StringUtils.isEmpty(filtro.getSku())) {
 				criteria.add(Restrictions.eq("sku", filtro.getSku()));
