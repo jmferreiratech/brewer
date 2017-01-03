582e514fe317d3afa906d1466dc464599243c01e
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-01-03 17:05:56 -0300

    Add customized validator to password

--- src/main/java/com/algaworks/brewer/model/Usuario.java
+++ src/main/java/com/algaworks/brewer/model/Usuario.java
@@ -13,11 +13,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+import com.algaworks.brewer.validation.AtributoConfirmacao;
+
+@AtributoConfirmacao(atributo = "senha", atributoConfirmacao = "confirmacaoSenha", message = "Confirmação da senha não confere")
 @Entity
 @Table(name = "usuario")
 public class Usuario implements Serializable {
@@ -35,6 +39,9 @@ public class Usuario implements Serializable {
 	private String email;
 
 	private String senha;
+
+	@Transient
+	private String confirmacaoSenha;
 
 	private Boolean ativo;
 
@@ -76,6 +83,14 @@ public class Usuario implements Serializable {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
 	}
 
 	public Boolean getAtivo() {
