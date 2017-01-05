1c54da610081d77cdbf3cd9869f5d489e5794510
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-01-05 12:49:57 -0300

    Loads users' permissions from db

--- src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -1,9 +1,14 @@
 package com.algaworks.brewer.security;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +29,13 @@ public class AppUserDetailsService implements UserDetailsService {
 		Optional<Usuario> usuarioOptional = usuarios.porEmailEAtivo(email);
 		Usuario usuario = usuarioOptional
 				.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
-		return new User(usuario.getEmail(), usuario.getSenha(), new HashSet<>());
+		return new User(usuario.getEmail(), usuario.getSenha(), getPermissoes(usuario));
+	}
+
+	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
+		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+		List<String> permissoes = usuarios.permissoes(usuario);
+		permissoes.forEach(p -> authorities.add(new SimpleGrantedAuthority(p.toUpperCase())));
+		return authorities;
 	}
 }
