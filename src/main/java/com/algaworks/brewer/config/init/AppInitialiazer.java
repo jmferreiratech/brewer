fa521c5b1c0fc0f4f90a24a6357c7c7187aeb5a8
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-01-05 15:01:17 -0300

    Fixes encoding bug due to security insertion

--- src/main/java/com/algaworks/brewer/config/init/AppInitialiazer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitialiazer.java
@@ -4,7 +4,6 @@ import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
 
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.brewer.config.JPAConfig;
@@ -31,10 +30,7 @@ public class AppInitialiazer extends AbstractAnnotationConfigDispatcherServletIn
 
 	@Override
 	protected Filter[] getServletFilters() {
-		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-		characterEncodingFilter.setEncoding("UTF-8");
-		characterEncodingFilter.setForceRequestEncoding(true);
-		return new Filter[] { characterEncodingFilter };
+		return new Filter[] {};
 	}
 
 	@Override
