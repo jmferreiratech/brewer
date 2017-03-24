be48da9aa40388c62ca680a0c92107fb842ac1a6
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-24 20:37:59 -0300

    Prototypes users search page

--- src/main/java/com/algaworks/brewer/controller/UsuariosController.java
+++ src/main/java/com/algaworks/brewer/controller/UsuariosController.java
@@ -1,17 +1,24 @@
 package com.algaworks.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.algaworks.brewer.controller.page.PageWrapper;
 import com.algaworks.brewer.model.Usuario;
 import com.algaworks.brewer.repository.Grupos;
+import com.algaworks.brewer.repository.Usuarios;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 import com.algaworks.brewer.service.CadastroUsuarioService;
 import com.algaworks.brewer.service.exception.EmailUsuarioJaCadastradoException;
 import com.algaworks.brewer.service.exception.SenhaObrigatoriaUsuarioException;
@@ -25,6 +32,9 @@ public class UsuariosController {
 
 	@Autowired
 	private Grupos grupos;
+
+	@Autowired
+	private Usuarios usuarios;
 
 	@RequestMapping("/novo")
 	public ModelAndView novo(Usuario usuario) {
@@ -49,5 +59,16 @@ public class UsuariosController {
 		}
 		attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
 		return new ModelAndView("redirect:/usuarios/novo");
+	}
+
+	@GetMapping
+	public ModelAndView pesquisar(UsuarioFilter usuarioFilter, BindingResult result,
+			@PageableDefault(size = 3) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("usuario/PesquisaUsuarios");
+		PageWrapper<Usuario> paginaWrapper = new PageWrapper<>(usuarios.filtrar(usuarioFilter, pageable),
+				httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		mv.addObject("grupos", grupos.findAll());
+		return mv;
 	}
 }
--- src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
+++ src/main/java/com/algaworks/brewer/repository/helper/usuario/UsuariosImpl.java
@@ -6,7 +6,12 @@ import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import com.algaworks.brewer.model.Usuario;
+import com.algaworks.brewer.repository.filter.UsuarioFilter;
 
 public class UsuariosImpl implements UsuariosQueries {
 
@@ -24,5 +29,11 @@ public class UsuariosImpl implements UsuariosQueries {
 		return manager.createQuery(
 				"select distinct p.nome from Usuario u inner join u.grupos g inner join g.permissoes p where u = :usuario",
 				String.class).setParameter("usuario", usuario).getResultList();
+	}
+
+	@Override
+	public Page<Usuario> filtrar(UsuarioFilter filtro, Pageable pageable) {
+		List<Usuario> results = manager.createQuery("from Usuario where ativo = true", Usuario.class).getResultList();
+		return new PageImpl<Usuario>(results, pageable, results.size());
 	}
 }
