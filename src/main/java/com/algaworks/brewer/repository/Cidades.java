4b346bd2180f5142d42f020a3c56c422259cd7f3
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-24 19:36:24 -0300

    Fixes (annotates) Cidades repository

--- src/main/java/com/algaworks/brewer/repository/Cidades.java
+++ src/main/java/com/algaworks/brewer/repository/Cidades.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.brewer.model.Cidade;
 import com.algaworks.brewer.model.Estado;
 import com.algaworks.brewer.repository.helper.cidade.CidadesQueries;
 
+@Repository
 public interface Cidades extends JpaRepository<Cidade, Long>, CidadesQueries {
 
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
