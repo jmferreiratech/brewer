0c875ac94ebbaf0df7e846d0d588f97aecd42558
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-24 14:11:03 -0300

    Fixes char encoding again

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -87,6 +87,7 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		resolver.setPrefix("classpath:/templates/");
 		resolver.setSuffix(".html");
 		resolver.setTemplateMode(TemplateMode.HTML);
+		resolver.setCharacterEncoding("UTF-8");
 		return resolver;
 	}
 
