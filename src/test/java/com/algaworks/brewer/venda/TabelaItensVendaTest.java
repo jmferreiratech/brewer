eda75fa466edc027096e878ddb1422783af21c5b
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-26 14:41:44 -0300

    Implements functionalities and adds tests to sell table

--- src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
+++ src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
@@ -1,11 +1,13 @@
 package com.algaworks.brewer.venda;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import com.algaworks.brewer.model.Cerveja;
 
 public class TabelaItensVendaTest {
 
@@ -19,5 +21,29 @@ public class TabelaItensVendaTest {
 	@Test
 	public void deveCalcularValorTotalSemItens() throws Exception {
 		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+
+	@Test
+	public void deveCalcularValorTotalComUmItem() throws Exception {
+		String valor = "8.90";
+		addCerveja(1, valor);
+
+		assertEquals(new BigDecimal(valor), tabelaItensVenda.getValorTotal());
+	}
+
+	@Test
+	public void deveCalcularValorTotalComVariosItens() throws Exception {
+		addCerveja(1, "8.90");
+		addCerveja(2, "4.99");
+
+		assertEquals(new BigDecimal("18.88"), tabelaItensVenda.getValorTotal());
+
+	}
+
+	private void addCerveja(Integer quantidade, String valor) {
+		Cerveja cerveja = new Cerveja();
+		BigDecimal val = new BigDecimal(valor);
+		cerveja.setValor(val);
+		tabelaItensVenda.adicionarItem(cerveja, quantidade);
 	}
 }
