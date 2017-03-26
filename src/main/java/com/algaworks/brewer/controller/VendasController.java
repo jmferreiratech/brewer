4aca93cb5fd525986e48175b7f5d85850eb22b80
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-26 16:48:12 -0300

    Adds sell units to sell panel with partial html rendering

--- src/main/java/com/algaworks/brewer/controller/VendasController.java
+++ src/main/java/com/algaworks/brewer/controller/VendasController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.repository.Cervejas;
@@ -26,10 +26,11 @@ public class VendasController {
 	}
 
 	@PostMapping("/item")
-	public @ResponseBody String adicionarItem(Long codigoCerveja) {
+	public ModelAndView adicionarItem(Long codigoCerveja) {
 		Cerveja cerveja = cervejas.findOne(codigoCerveja);
 		tabelaItensVenda.adicionarItem(cerveja, 1);
-		System.out.println(">>> total de itens: " + tabelaItensVenda.size());
-		return "Item adicionado";
+		ModelAndView mv = new ModelAndView("venda/TabelaItensVenda");
+		mv.addObject("itens", tabelaItensVenda.getItens());
+		return mv;
 	}
 }
--- src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
+++ src/main/java/com/algaworks/brewer/session/TabelaItensVenda.java
@@ -32,4 +32,8 @@ public class TabelaItensVenda {
 	public int size() {
 		return itens.size();
 	}
+
+	public List<ItemVenda> getItens() {
+		return itens;
+	}
 }
