4c03b16e1e3529af750ab9cbfa7f8db0968a52f5
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-26 14:14:08 -0300

    Adds JUnit alongside with sell item and sell table

---
+++ src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
@@ -0,0 +1,23 @@
+package com.algaworks.brewer.venda;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TabelaItensVendaTest {
+
+	private TabelaItensVenda tabelaItensVenda;
+
+	@Before
+	public void setUp() {
+		tabelaItensVenda = new TabelaItensVenda();
+	}
+
+	@Test
+	public void deveCalcularValorTotalSemItens() throws Exception {
+		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+}
