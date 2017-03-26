4b070d9c3d3507159cae24bed8d1e84169635c21
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-25 22:15:56 -0300

    Implements clients quick search

--- src/main/java/com/algaworks/brewer/repository/Clientes.java
+++ src/main/java/com/algaworks/brewer/repository/Clientes.java
@@ -1,5 +1,6 @@
 package com.algaworks.brewer.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,6 @@ import com.algaworks.brewer.repository.helper.cliente.ClientesQueries;
 public interface Clientes extends JpaRepository<Cliente, Long>, ClientesQueries {
 
 	public Optional<Cliente> findByCpfOuCnpj(String cpfOuCnpj);
+
+	public List<Cliente> findByNomeStartingWithIgnoreCase(String nome);
 }
