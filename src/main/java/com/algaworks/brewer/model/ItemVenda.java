30215a95fece479377f9668bfc9576c9d4a8b20c
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-03-28 22:38:23 -0300

    Maps sell tables

--- src/main/java/com/algaworks/brewer/model/ItemVenda.java
+++ src/main/java/com/algaworks/brewer/model/ItemVenda.java
@@ -1,13 +1,39 @@
 package com.algaworks.brewer.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class ItemVenda {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "item_venda")
+public class ItemVenda implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+
 	private Integer quantidade;
+
+	@Column(name = "valor_unitario")
 	private BigDecimal valorUnitario;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_cerveja")
 	private Cerveja cerveja;
+
+	@ManyToOne
+	@JoinColumn(name = "codigo_venda")
+	private Venda venda;
 
 	public Long getCodigo() {
 		return codigo;
@@ -39,6 +65,14 @@ public class ItemVenda {
 
 	public void setCerveja(Cerveja cerveja) {
 		this.cerveja = cerveja;
+	}
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
 	}
 
 	public BigDecimal getValorTotal() {
