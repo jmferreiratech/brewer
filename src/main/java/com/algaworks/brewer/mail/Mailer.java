5c1b6b1388cffcb24f8d1f4495fb53c879afbb49
Marcelo Ferreira <joaomarcelo@lesc.ufc.br>
2017-04-03 22:58:00 -0300

    Sends simple emails

--- src/main/java/com/algaworks/brewer/mail/Mailer.java
+++ src/main/java/com/algaworks/brewer/mail/Mailer.java
@@ -1,12 +1,27 @@
 package com.algaworks.brewer.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import com.algaworks.brewer.model.Venda;
 
 @Component
 public class Mailer {
 
+	@Autowired
+	private JavaMailSender mailSender;
+
 	@Async
-	public void enviar() {
+	public void enviar(Venda venda) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("joamarcelo@gmail.com");
+		message.setTo("fortmarcelo@yahoo.com.br");
+		message.setSubject("Venda efetuada com sucesso");
+		message.setText("Obrigado, sua venda foi processada!");
+
+		mailSender.send(message);
 	}
 }
